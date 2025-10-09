ef29bf7ef4424f2595830c25a7d936bd4319f326
raxxmax <raahulshaarmaa28@gmail.com>
2025-10-09 23:10:24 +0530

    imports change

--- app/src/main/java/com/example/easybuy/feature/likedProducts/likedProductsScreen.kt
+++ app/src/main/java/com/example/easybuy/feature/likedProducts/likedProductsScreen.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
