b9c88b07803a5864044fec2f0048f6aaa80e9a60
raxxmax <raahulshaarmaa28@gmail.com>
2025-10-09 23:14:46 +0530

    Completed UI part

--- app/src/main/java/com/example/easybuy/feature/likedProducts/likedProductsScreen.kt
+++ app/src/main/java/com/example/easybuy/feature/likedProducts/likedProductsScreen.kt
@@ -1,29 +1,21 @@
 import android.R.attr.onClick
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
-import com.example.easybuy.R
-import com.example.easybuy.feature.home.BottomNavigationBar
 import com.example.easybuy.feature.home.ProductCard
 
 
